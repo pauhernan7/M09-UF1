c6146dc427d54671e5a77d919a2b7d901c2009e5
Pau Hernandez <2023_pau.hernandez@iticbcn.cat>
2024-09-25 15:46:27 +0200

    completat Rot13 sense funcions

--- exercici_1/Rot13.java
+++ exercici_1/Rot13.java
@@ -7,46 +7,105 @@ public class Rot13 {
        
         String cadena = "";
         String cadena2 = "";
+        String cadenaDesxifrat = "";
         int posicio = 0; 
-        boolean tipusLletra;
 
-        System.out.println("Text a xifrar?");
-        String entrada = Entrada.readLine();   // sola
+        System.out.println("Que vols xifrar o desxifrar?");
+        String opcio = Entrada.readLine();
+        String entrada = "";   // sola
 
-        for (int i = 0; i <= entrada.length()-1; i++) {
-            if(Character.isUpperCase(entrada.charAt(i))) {
-                for(int z = 0; z < lletresMaj.length; z++ ) {
-                    if(entrada.charAt(i) == lletresMaj[z]) {
-                        if((posicio + z + 13) > lletresMaj.length) {
-                            int correcio = posicio + z + 13 - lletresMaj.length;
-                           cadena = cadena + lletresMaj[correcio];
-                        } 
-                        else {
-                            int correcio2 = z + 13;
-                            cadena = cadena + lletresMaj[correcio2];
+
+        if (opcio.equals("xifrar")) {
+            System.out.println("Text a xifrar?");
+            entrada = Entrada.readLine();
+            for (int i = 0; i <= entrada.length()-1; i++) {
+                if(Character.isUpperCase(entrada.charAt(i))) {
+                    for(int z = 0; z < lletresMaj.length; z++ ) {
+                        if(entrada.charAt(i) == lletresMaj[z]) {
+                            if((posicio + z + 13) > lletresMaj.length) {
+                                int correcio = posicio + z + 13 - lletresMaj.length;
+                            cadena = cadena + lletresMaj[correcio];
+                            } 
+                            else {
+                                int correcio2 = z + 13;
+                                cadena = cadena + lletresMaj[correcio2];
+                            }
                         }
                     }
-                 }
-            }
-            else {
-                for(int z = 0; z < lletresMin.length; z++ ) {
-                    if(entrada.charAt(i) == lletresMin[z]) {
-                        if((posicio + z + 13) > lletresMin.length) {
-                            int correcio = posicio + z + 13 - lletresMin.length;
-                           cadena = cadena + lletresMin[correcio];
-                        } 
-                        else {
-                            int correcio2 = z + 13;
-                            cadena = cadena + lletresMin[correcio2];
+                }
+                
+                
+                else {
+                    for(int z = 0; z < lletresMin.length; z++ ) {
+                        if(entrada.charAt(i) == lletresMin[z]) {
+                            if((posicio + z + 13) > lletresMin.length) {
+                                int correcio = posicio + z + 13 - lletresMin.length;
+                            cadena = cadena + lletresMin[correcio];
+                            } 
+                            else {
+                                int correcio2 = z + 13;
+                                cadena = cadena + lletresMin[correcio2];
+                            }
                         }
                     }
                 }
             }
-            
+            System.out.println(cadena);  
+            System.out.println("");
+        }
+        else if(opcio.equals("desxifrar")) {
+            System.out.println("Text a desxifrar?");
+            entrada = Entrada.readLine();
+            for(int i = 0; i <= entrada.length()-1; i++) {
+                if(Character.isUpperCase(entrada.charAt(i))) {
+                    for(int z = 0; z < lletresMaj.length; z++) {
+                        if(entrada.charAt(i) == lletresMaj[z]) {
+                            if(z - 13 < 0) {
+                                int correcio3 = z - 13 + lletresMaj.length;
+                                cadenaDesxifrat = cadenaDesxifrat + lletresMaj[correcio3];
+                            }
+                            else {
+                                int correcio3 = z - 13;
+                                cadenaDesxifrat = cadenaDesxifrat + lletresMaj[correcio3];
+                            }
+                        }
+
+                    }
+                }
+                else {
+                    if(Character.isLowerCase(entrada.charAt(i))) {
+                        for(int z = 0; z < lletresMin.length; z++) {
+                            if(entrada.charAt(i) == lletresMin[z]) {
+                                if(z - 13 < 0) {
+                                    int correcio3 = z - 13 + lletresMin.length;
+                                    cadenaDesxifrat = cadenaDesxifrat + lletresMin[correcio3];
+                                }
+                                else {
+                                    int correcio4 = z - 13;
+                                    cadenaDesxifrat = cadenaDesxifrat + lletresMin[correcio4];
+                                }
+                            }
+    
+                        }
+                    }
+                }
+
+            }
+            System.out.println(cadenaDesxifrat);  
+            System.out.println("");
 
         }
-        System.out.println(cadena);  
-        System.out.println("");
+        else {
+            System.out.println("Només pots " + "'" + "xifrar" + "'" + " o " + "'" + "desxifrar" + "'" +".");
+        }
+
+        
+
+
+        
+        
+
+
     }
 
 }
