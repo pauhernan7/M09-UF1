101d330e384cc717c2108e1bfce5f630227f59fc
Pau Hernandez <2023_pau.hernandez@iticbcn.cat>
2024-09-26 18:26:29 +0200

    fet amb 2 mètodes

--- exercici_1/Rot13.java
+++ exercici_1/Rot13.java
@@ -7,39 +7,30 @@ public class Rot13 {
     public static final char [] lletresMin = {'a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'v', 'w', 'x', 'y', 'z'};
     public static final char [] lletresMaj = {'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'W', 'X', 'Y', 'Z'};
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  
+    public static String xifraRot13 (String entrada) {  
 
-        String cadena = "";
-        String cadena2 = "";
-        String cadenaDesxifrat = "";
-        int posicio = 0; 
-
-        System.out.println("Que vols xifrar o desxifrar?");
-        String opcio = scanner.nextLine();  
-        String entrada = "";   
-
-        if (opcio.equals("xifrar")) {
-            System.out.println("Text a xifrar?");
-            entrada = scanner.nextLine();  // Usamos scanner para la entrada del texto a cifrar
+        String cadena = "";   
+         
             for (int i = 0; i <= entrada.length()-1; i++) {
                 if(Character.isUpperCase(entrada.charAt(i))) {
                     for(int z = 0; z < lletresMaj.length; z++ ) {
                         if(entrada.charAt(i) == lletresMaj[z]) {
-                            if((posicio + z + 13) > lletresMaj.length) {
-                                int correcio = posicio + z + 13 - lletresMaj.length;
+                            if(( + z + 13) > lletresMaj.length) {
+                                int correcio = z + 13 - lletresMaj.length;
                                 cadena = cadena + lletresMaj[correcio];
-                            } else {
+                            } 
+                            else {
                                 int correcio2 = z + 13;
                                 cadena = cadena + lletresMaj[correcio2];
                             }
                         }
                     }
-                } else {
+                } 
+                else {
                     for(int z = 0; z < lletresMin.length; z++ ) {
                         if(entrada.charAt(i) == lletresMin[z]) {
-                            if((posicio + z + 13) > lletresMin.length) {
-                                int correcio = posicio + z + 13 - lletresMin.length;
+                            if(( z + 13) > lletresMin.length) {
+                                int correcio = z + 13 - lletresMin.length;
                                 cadena = cadena + lletresMin[correcio];
                             } else {
                                 int correcio2 = z + 13;
@@ -49,11 +40,15 @@ public class Rot13 {
                     }
                 }
             }
-            System.out.println(cadena);  
-            System.out.println("");
-        } else if(opcio.equals("desxifrar")) {
-            System.out.println("Text a desxifrar?");
-            entrada = scanner.nextLine();  // Usamos scanner para la entrada del texto a descifrar
+            return cadena;  
+    } 
+        
+       
+        // desxifraRot13
+        public static String desxifraRot13 (String entrada) {
+
+            String cadenaDesxifrat = "";
+
             for(int i = 0; i <= entrada.length()-1; i++) {
                 if(Character.isUpperCase(entrada.charAt(i))) {
                     for(int z = 0; z < lletresMaj.length; z++) {
@@ -67,14 +62,16 @@ public class Rot13 {
                             }
                         }
                     }
-                } else {
+                } 
+                else {
                     if(Character.isLowerCase(entrada.charAt(i))) {
                         for(int z = 0; z < lletresMin.length; z++) {
                             if(entrada.charAt(i) == lletresMin[z]) {
                                 if(z - 13 < 0) {
                                     int correcio3 = z - 13 + lletresMin.length;
                                     cadenaDesxifrat = cadenaDesxifrat + lletresMin[correcio3];
-                                } else {
+                                } 
+                                else {
                                     int correcio4 = z - 13;
                                     cadenaDesxifrat = cadenaDesxifrat + lletresMin[correcio4];
                                 }
@@ -83,12 +80,34 @@ public class Rot13 {
                     }
                 }
             }
-            System.out.println(cadenaDesxifrat);  
-            System.out.println("");
-        } else {
+            return cadenaDesxifrat;  
+        }
+
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Que vols xifrar o desxifrar?");
+        String opcio = scanner.nextLine();
+
+        if (opcio.equals("xifrar")) {
+            System.out.println("Text a xifrar?");
+            String entrada = scanner.nextLine();
+            String guardarXifrat = xifraRot13(entrada);
+            System.out.println(guardarXifrat);
+        }
+        else if(opcio.equals("desxifrar")) {
+            System.out.println("Text a desxifrar?");
+            String entrada = scanner.nextLine();
+            String guardarDesxifrat = desxifraRot13(entrada);
+            System.out.println(guardarDesxifrat);
+        }
+        else {
             System.out.println("Només pots " + "'" + "xifrar" + "'" + " o " + "'" + "desxifrar" + "'" + ".");
         }
 
-        scanner.close();  // Cerramos el scanner al finalizar el programa
+        scanner.close();
+
     }
 }
